7be242b863debf5100c8949b4c58c16fc0de1300
maryam galal <119633940+maryam-galal@users.noreply.github.com>
2023-12-06 02:35:33 +0200

    commit

--- src/main/java/com/example/mainpage/LoginController.java
+++ src/main/java/com/example/mainpage/LoginController.java
@@ -4,29 +4,27 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import java.io.IOException;
 import javafx.scene.control.Label;
 public class LoginController {
     @FXML
     private Button login;
     @FXML
-    private PasswordField password_check;
-
+    private PasswordField passwordField;
     @FXML
-    private TextField username_check;
+    private TextField usernameField;
     @FXML
     private Label invalid_massage;
     @FXML
     void logincheck(MouseEvent event) {
-        String enteredPassword = password_check.getText();
-        String enteredUsername=username_check.getText();
+        String enteredPassword = passwordField.getText();
+        String enteredUsername= usernameField.getText();
         if(enteredPassword.equals("administrator1")&&enteredUsername.equals("maryam")){
             System.out.println("log in successfully!");
         }
         if(enteredPassword.length()<10){
             System.out.println("invalid password");
             invalid_massage.setVisible(true);
-            password_check.setText("");
+            passwordField.setText("");
         }
 
     }
