e2689b2c98e8ef74a5f89d0aaea388cfe08d622d
maryam galal <119633940+maryam-galal@users.noreply.github.com>
2023-12-06 03:35:21 +0200

    commit

--- src/main/java/com/example/mainpage/LoginController.java
+++ src/main/java/com/example/mainpage/LoginController.java
@@ -5,7 +5,11 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import java.util.*;
+import java.io.*;
+
 public class LoginController {
+    private List<String[]> userList = new ArrayList<>();
     @FXML
     private Button login;
     @FXML
@@ -15,17 +19,55 @@ public class LoginController {
     @FXML
     private Label invalid_massage;
     @FXML
-    void logincheck(MouseEvent event) {
+    void logincheck(MouseEvent event) throws IOException {
         String enteredPassword = passwordField.getText();
-        String enteredUsername= usernameField.getText();
-        if(enteredPassword.equals("administrator1")&&enteredUsername.equals("maryam")){
-            System.out.println("log in successfully!");
+        String enteredUsername = usernameField.getText();
+        boolean loginSuccessful = false;
+        try (Scanner fileScanner = new Scanner(new File("C:\\Users\\Mariam\\IdeaProjects\\mainpage\\src\\main\\java\\com\\example\\mainpage\\login.csv"))) {
+            while (fileScanner.hasNextLine()) {
+                String[] data = fileScanner.nextLine().split(",");
+                userList.add(data); // Add each row to the list
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        if(enteredPassword.length()<10){
-            System.out.println("invalid password");
+
+        for (String[] data : userList) {
+            if (data.length >= 2) {
+                String usernameFromFile = data[0].trim();
+                String passwordFromFile = data[1].trim();
+                String userType = data[2].trim();
+                if (enteredUsername.equals(usernameFromFile) && enteredPassword.equals(passwordFromFile)) {
+                    // Matching username and password found
+                    loginSuccessful = true;
+                    if(userType.equals("coach")){
+                        System.out.println("coach");
+                        MainApplication CoachMain=new MainApplication();
+                        CoachMain.changeScene("coachPage.fxml");
+                    }
+                    else if (userType.equals("customer")) {
+                        System.out.println("customer");
+                        MainApplication CustomerMain=new MainApplication();
+                        CustomerMain.changeScene("customerPage.fxml");
+                    } else if (userType.equals("admin")) {
+                        System.out.println("admin");
+                        MainApplication AdminMain=new MainApplication();
+                        AdminMain.changeScene("adminPage.fxml");
+                    }
+                    break;
+                }
+
+            }
+        }
+
+        if (loginSuccessful) {
+            System.out.println("Login successful!");
+            // Add your code here for what should happen after successful login
+        } else {
+            System.out.println("Invalid username or password");
             invalid_massage.setVisible(true);
             passwordField.setText("");
         }
-
     }
 }
+
